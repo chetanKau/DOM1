785633a1b365960e248c10567018f8adf3c520c0
Chetan kaushik <chetankaushik29@gmail.com>
2024-01-12 15:13:04 +0530

    Dom 1st project is created

---
+++ index.js
@@ -0,0 +1,9 @@
+let mainDiv=document.getElementById("main");
+let para=document.querySelector("p");
+console.log(mainDiv);
+
+mainDiv.style.backgroundColor="#FFFF00";
+para.innerText="Hello, I'm a div!"
+para.style.color="#000000"
+mainDiv.style.margin="1rem";
+para.style.padding="5px";
